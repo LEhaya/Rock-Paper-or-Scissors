b616a74d23acff83de15ab8fb973cf88ce7baab8
LEhaya <49375913+LEhaya@users.noreply.github.com>
2019-04-17 21:42:38 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,36 +1,25 @@
+/*
+synes denne kjempe fint jeg 
+Kod koden din her
+Dette er de forskjellige kodene du kan bruke for å gjøre
+diverse ting:
 
-function debuggingTool() {
-    this.write = function(txt) {
-        var parent = document.getElementById("log");
-        
-        var el = document.createElement("div");
-        el.setAttribute("id", "log-element");
-        el.setAttribute("class", "log-element");
-         var p = document.createElement("p");
-         p.innerHTML = txt;
-
-         var items = document.getElementsByClassName("log-element");
-
-         if(items) {
-            var l = items.length;
-            if(l % 2 == 0) {
-              //  p.setAttribute("style", "background-color: rgb(160,120,20);");
-            } else {
-                p.setAttribute("style", "background-color: rgb(179, 198, 255);")
-            }
-         }
+En erstatning for console.log(); er:
+teller.write("teksten din her");
+*/
 
 
 
-         el.appendChild(p);
 
-         parent.appendChild(el);
 
+//SLUTT PÅ KODEN
+
+    
+    
+    try {
+        code();
     }
-}
-
-
-const teller = new debuggingTool();
-
-
+    catch(err) {
+        teller.write("Error!: " + err.message);
+    }
 
